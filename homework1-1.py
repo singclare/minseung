bb7c102a2c5fe3b9f7a9e550cd1c1ca1937de946
singclare <grochi1995@gmail.com>
2019-06-11 21:22:33 +0900

    '19.06.11
    Minseung's task 1-1

---
+++ homework1-1.py
@@ -0,0 +1,75 @@
+##Print()##
+#Program 2-1#
+print('부산대학교')
+print('SW교육센터')
+print('컴퓨팅사고')
+
+##Program 2-2#
+print("부산대학교")
+print("SW교육센터")
+print("컴퓨팅사고")
+
+##Program 2-3#
+print("""One
+Two
+Three""")
+
+##Comments##
+#Program 2-4#
+#화면에 여러 줄을
+#출력하는 방법에 대한 코드입니다.
+print("부산대학교")
+print("SW교육센터")
+print("컴퓨팅사고")
+
+#Program 2-5#
+print("부산대학교") #첫 번째 줄에 '부산대학교'를 출력합니다.
+print("SW교육센터") #두 번째 줄에 'SW교육센터'를 출력합니다.
+print("파이썬 프로그래밍 기초") #세 번째 줄에 '파이썬 프로그래밍 기초'를 출력합니다.
+
+##Variables##
+#Program 2-6#
+#하나의 변수를 활용하는 코드입니다.
+Lab = 105
+print("우리가 수업하는 실습실:")
+print(Lab)
+
+#program 2-7#
+#하나의 변수를 활용하는 코드입니다.
+Lab = 105
+print('우리가 수업하는 실습실: ', Lab)
+
+#Program 2-8#
+#변수 두 개를 생성하고 값을 할당
+my_id = 20150001
+grade = 1
+#두 개의 변수에 할당된 값을 출력
+print('나의 학번: ')
+print(my_id)
+print('학년: ')
+print(grade)
+
+#Program 2-9#
+#변수에 값을 할당하고 출력하는 예제입니다.
+#dollars 변수에 값을 할당해봅시다.
+dollars = 2.75
+print('I have', dollars, 'in my account')
+#dollars 변수에 새로운 값을 할당해봅시다.
+dollars = 99.95
+print('But now I have', dollars, 'in my account!')
+
+#Program 2-10#
+#두 개의 변수를 생성하고 문자열을 할당합니다.
+department = '국어국문과'
+name = '홍길동'
+#각 변수에 할당된 문자열 값 출력
+print(department, name)
+
+##INPUT() 함수를 이용하여 키보드로부터 입력받기##
+#Program 2-11#
+#학과명을 입력받습니다.
+department = input('학과명: ')
+#여러분의 이름을 입력받습니다.
+name = input('성명: ')
+#두 변수 값을 출력합니다.
+print('안녕하세요', department, name)
